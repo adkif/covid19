fbe6424a1d5c7d469b82334c7572da18c602fac0
Adolphe Kifungo <akifungo@gmail.com>
2020-05-06 22:57:35 +0200

    deploye to heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/covid19-drc'));
+app.use(express.static('./dist/covid19'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/covid19-drc/' }),
+    res.sendFile('index.html', { root: 'dist/covid19/' }),
 );
 
 app.listen(process.env.PORT || 8080);
